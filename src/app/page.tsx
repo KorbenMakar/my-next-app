ba5aa373ea0008bdfae8f790cb883c0ac6224b0e
KorbenMakar <korbenmakar@gmail.com>
2024-12-18 12:37:51 +0200

    New project has been started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-import styles from "./page.module.css";
-import course from "../course.json"
-
-export default function Home() {
+export default async function Home() {
     return (
-        <div className={styles.page}>
-            <main className={styles.main}>
-                <ul>
-                    {course.lessons.map((lesson) => {
-                        return (
-                            <li key={lesson.name}>
-                                <h2>{lesson.name}</h2>
-                                <p>{lesson.title}</p>
-                            </li>
-                        );
-                    })
-                    }
-                </ul>
-            </main>
-        </div>
+        <main>
+
+        </main>
     );
 }
